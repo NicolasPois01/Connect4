d215561f42438b979d0b6f62749008536e363942
NicolasPois01 <pois.nicolas01@gmail.com>
2022-01-03 11:57:49 +0100

    Initialisation matrice

--- Model.js
+++ Model.js
@@ -1,3 +1,24 @@
 class Model {
-    constructor(){}
+    matrix = new Array(7);
+    constructor(){
+        for (let i = 0; i < 7; ++i){
+            this.matrix[i] = new Array(7);
+        }
+        for (let i = 0; i < 7; ++i){
+            for (let j = 0; j < 6; ++j){
+                this.matrix[i][j] = 0;
+            }
+        }
+    }
+
+    showMatrix(){
+        for (let i = 0; i < 7; ++i){
+            for (let j = 0; j < 6; ++j){
+                console.log(this.matrix[i][j]);
+            }
+        }
+    }
 }
+
+const model = new Model();
+model.showMatrix();
